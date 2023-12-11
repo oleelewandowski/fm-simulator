abe43ec6278f19eebdd0d3cbe26f6371893d1bc2
Oliver <oliver.lewandowski1@gmail.com>
2023-12-11 19:03:05 +0100

    build: vercel

--- BE/simulator_backend/simulator_backend/wsgi.py
+++ BE/simulator_backend/simulator_backend/wsgi.py
@@ -16,3 +16,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simulator_backend.settings')
 
 # Tworzenie i konfiguracja aplikacji WSGI
 application = get_wsgi_application()
+
+app = application
